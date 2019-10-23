2cf62172fcc5d6132f65aa74ef69dbcce8c18b7e
okirsh <okirsh@cadence.com>
2019-10-23 13:07:22 +0300

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,10 @@
+
+#set the Python Include directory
+#setenv SPECMAN_PYTHON_INCLUDE_DIR /<ROOT>/include/python2.7
+
+#set the Python lib directory
+#setenv SPECMAN_PYTHON_LIB_DIR /<ROOT>/lib/python2.7/config-x86_64-linux-gnu
+
+#set the location of the Python module: plot_i.py
+#setenv  PYTHONPATH ~/<file path>
+
